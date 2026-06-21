a024ea02acb36ccf109e4296548439650be0d586
SREERUP MOJUMDER <vhopel.sm@gmail.com>
2026-06-21 22:36:05 +0600

    Add asset path utility and update splash screen imports

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:task_manager/utils/asset_path.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
