20935735d9dd814013b25f1d8fe666ee123b3b60
SREERUP MOJUMDER <vhopel.sm@gmail.com>
2026-06-21 22:30:01 +0600

    Add Windows runner support with DPI awareness and console output
    - Introduced a manifest file for the Windows runner to enable PerMonitorV2 DPI awareness.
    - Implemented utility functions for creating a console and handling command line arguments in utils.cpp and utils.h.
    - Developed a Win32Window class to manage window creation, message handling, and theme updates based on system preferences.
    - Added necessary header files and resource management for the Win32 window.

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager/screens/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task Manager',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: SplashScreen(),
+    );
+  }
+}
